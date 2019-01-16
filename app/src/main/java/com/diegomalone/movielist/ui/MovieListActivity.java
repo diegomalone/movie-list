744cdc1efaf2564d8b07f5a2e1b40ebaae5f6722
Diego Malone <diegomalone@gmail.com>
2019-01-15 22:54:48 -0200

    Create style and main activity

---
+++ app/src/main/java/com/diegomalone/movielist/ui/MovieListActivity.java
@@ -0,0 +1,14 @@
+package com.diegomalone.movielist.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.diegomalone.movielist.R;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+    }
+}
